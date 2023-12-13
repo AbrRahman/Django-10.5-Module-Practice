e75f4450e81d8949d99ba5f52872699abc44a9e9
AbrRahman <abrahman.est@gamil.com>
2023-12-13 22:05:19 +0600

    add templates filter

--- sample_page_navigation/views.py
+++ sample_page_navigation/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-def home(req):
-    return render(req,'home.html')
+import datetime
+def home(request):
+    student={"name":"Fahim","dec":"Fahim Al Imran is a Studen","age":16,"subject":["Bangla","English","Math","ICT"],"date":datetime.datetime.now()}
+    return render(request,'home.html',student)
